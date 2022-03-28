56f52fbba448cbf77561bbcaf6907ff3408f7174
ajaygrylls <74204899+ajaygrylls@users.noreply.github.com>
2022-03-28 17:49:16 +0530

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -15,9 +15,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/arrow_avicii.mk
+    $(LOCAL_DIR)/evolution_avicii.mk
 
 COMMON_LUNCH_CHOICES := \
-    arrow_avicii-user \
-    arrow_avicii-userdebug \
-    arrow_avicii-eng
+    evolution_avicii-user \
+    evolution_avicii-userdebug \
+    evolution_avicii-eng
